954a698b3fcbd90c2b95c1f554fcd88a139f7ebd
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-09-15 11:51:21 +0900

    Update navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,6 +17,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
+        <x-navbar></x-navbar>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
