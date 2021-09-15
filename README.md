c8712f648b82ebae6f0eae0ffb2a1301ae22199f
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-09-15 11:02:22 +0900

    update video totiral in README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn_laravel_livewire_2
  Learn Laravel Livewire #2
+
+
+## Tutorial Video
+Laravel Livewire Blog Project Tutorial 2021 with Tailwindcss
+https://www.youtube.com/watch?v=S4sfeAvh_pA
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    auth()->user()->assignRole('admin');
+    // auth()->user()->assignRole('admin');
     return view('welcome');
 });
 
